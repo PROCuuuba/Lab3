14ce08e2436f218db433f5a25d8676346ed28edc
PROCuuuba <barkalov.ilia@gmail.com>
2024-09-30 10:11:57 +0300

    1

---
+++ Homework.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int milk = 4;
+    float a = 5;
+    float x = 55;
+
+    float result = (x + a) * (float)milk * (float)30;
+
+    printf("%f\n", result);
+}
